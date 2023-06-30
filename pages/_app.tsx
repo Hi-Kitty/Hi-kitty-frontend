ca8fed054752384a83be0c77e9f1582c09bcdd4c
yeonkr <1714882@sookmyung.ac.kr>
2023-06-30 11:36:13 +0900

    feat: 프로젝트 초기 설정

---
+++ pages/_app.tsx
@@ -0,0 +1,53 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
+import { globalStyles } from '../styles/Globalstyle';
+import { Global } from '@emotion/react';
+import { reset } from '../styles/style';
+import styled from '@emotion/styled';
+
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const STALE_TIME = 10 * 60 * 1000;
+  const CACHE_TIME = 10 * 60 * 1000;
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: STALE_TIME,
+            cacheTime: CACHE_TIME,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <>
+      {globalStyles}
+      <RecoilRoot>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>하이키티</title>
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <QueryClientProvider client={queryClient}>
+          <Global styles={reset} />
+          <Wrapper>
+            <Component {...pageProps} />
+          </Wrapper>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 500px;
+  max-width: 500px;
+  min-height: 100vh;
+  background-color: white;
+`;
---
+++ styles/style.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const reset = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'pretendard';
+    list-style: none;
+  }
+`;
+
+export const flextCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
