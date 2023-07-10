566299b6aaa22fab0cfcf3e115d8fc77dd2edb7c
yeonkr <1714882@sookmyung.ac.kr>
2023-07-10 23:26:24 +0900

    design: footer svg 추가 및 수정

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -1,29 +1,22 @@
 import styled from '@emotion/styled';
-import { useRouter } from 'next/router';
 import { colors } from '../../../styles/colors';
 import Image from 'next/image';
 
 export default function Footer() {
-  const router = useRouter();
-
   return (
     <FooterBoxWrpper>
       <FooterWrapper>
         <FooterLogo href="/">
-          <Image src="../../../../images/HI_KITTY_white.svg" width={110} height={28} alt="white_logo" />
+          <Image src="/images/Hikitty-white.svg" width={110} height={28} alt="white_logo" />
         </FooterLogo>
         <FooterIcon>
-          <a href="#">
-            <Image id="m" src="../../../../images/kakaotalk.svg" width={25} height={25} alt="kakaotalk_logo" />
-          </a>
-          <a href="#">
-            <Image src="../../../../images/instagram.svg" width={33} height={33} alt="instagram_logo" />
-          </a>
+          <Image src="/images/Kakaotalk.svg" width={33} height={25} alt="kakaotalk_logo" style={{ marginBottom: 3 }} />
+          <Image src="/images/Instagram.svg" width={33} height={33} alt="instagram_logo" />
         </FooterIcon>
         <FooterService>
-          <a href="#">공식노션보기</a> | <a href="#">도움말</a> | <a href="#">이용약관</a>
+          <a>공식노션보기</a> | <a>도움말</a> | <a>이용약관</a>
+          <Copyright>©Copyright. 2023 HI KITTY all rights reserved.</Copyright>
         </FooterService>
-        <Copyright>©Copyright. 2023 HI KITTY all rights reserved.</Copyright>
       </FooterWrapper>
     </FooterBoxWrpper>
   );
@@ -45,25 +38,20 @@ const FooterLogo = styled.a`
 `;
 const FooterIcon = styled.div`
   display: inline;
-
-  #m {
-    margin: 0 3px 3px 0;
-  }
 `;
 
 const FooterService = styled.div`
   margin-top: 15px;
   font-size: 11px;
-  color: ${colors.white};
+  color: ${colors.gray400};
 
   a {
-    color: ${colors.white};
     text-decoration: none;
+    letter-spacing: -0.2px;
   }
 `;
 
 const Copyright = styled.div`
   margin-top: 10px;
   font-size: 9px;
-  color: ${colors.white};
 `;
