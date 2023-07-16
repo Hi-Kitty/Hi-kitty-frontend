860e5cacf3e97d5492f1ae6ea313ec32607aa400
yeonkr <1714882@sookmyung.ac.kr>
2023-07-17 05:27:10 +0900

    refactor: 회원가입 리액트쿼리로 리팩토링

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -5,168 +5,164 @@ import Input from '../../components/Input';
 import BottomButton from '../../components/BottomButton';
 import Header from '../../components/Layout/Header';
 import { colors } from '../../styles/colors';
-import SignUpBox from './SignupBox/Box';
-import instance from '../../api/axios';
+import DetailBox from '../../components/DetailBox';
+import { useCreate } from '../../orval/api/유저-기부자-모금자-공통-api/유저-기부자-모금자-공통-api';
 
 export default function Signup() {
   const router = useRouter();
   const [mode, setMode] = useState<'ROLE_DONER' | 'ROLE_FUNDRAISER'>('ROLE_DONER');
-  const [nickname, setNickname] = useState({ value: '', text: '', hidden: true });
-  const [password, setPassword] = useState({ value: '', text: '', hidden: true });
-  const [passwordCheck, setPasswordCheck] = useState({ value: '', text: '', hidden: true });
-  const [email, setEmail] = useState({ value: '', text: '', hidden: true });
+  const [fields, setFields] = useState({
+    email: '',
+    nickname: '',
+    password: '',
+    passwordCheck: '',
+  });
+  const [errors, setErrors] = useState({
+    email: '',
+    nickname: '',
+    password: '',
+    passwordCheck: '',
+  });
 
-  const onChangePasswordCheck = (e: any) => {
-    setPasswordCheck({
-      value: e.target.value,
-      text: '',
-      hidden: e.target.value === password.value,
-    });
+  const createMutation = useCreate();
+
+  const validateField = (value: string, fieldName: string) => {
+    type FieldValidation = {
+      regex?: RegExp;
+      emptyErrorText: string;
+      regexErrorText: string;
+    };
+
+    const fieldValidations: Record<string, FieldValidation> = {
+      email: {
+        regex: /^[A-Za-z0-9]+@[^\s@]+\.[^\s@]+$/,
+        emptyErrorText: '이메일을 입력해주세요.',
+        regexErrorText: '이메일 주소를 정확히 입력해주세요.',
+      },
+      nickname: {
+        regex: /^[가-힣a-zA-Z]{2,10}$/,
+        emptyErrorText: '닉네임을 입력해주세요. (10글자 이하)',
+        regexErrorText: '한글/영문으로 입력해주세요. (10글자 이하)',
+      },
+      password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\S{8,}$/,
+        emptyErrorText: '비밀번호를 입력해주세요. (8글자 이상)',
+        regexErrorText: '공백없이 숫자, 대문자, 소문자가 하나씩 포함되어야 합니다. (8글자 이상)',
+      },
+      passwordCheck: {
+        emptyErrorText: '비밀번호 확인을 입력해주세요.',
+        regexErrorText: '비밀번호가 일치하지 않습니다.',
+      },
+    };
+
+    if (value.length === 0 || value === '') {
+      setErrors(prevErrors => ({ ...prevErrors, [fieldName]: fieldValidations[fieldName].emptyErrorText }));
+    } else {
+      const { regex, regexErrorText } = fieldValidations[fieldName];
+
+      if (regex && !regex.test(value)) {
+        setErrors(prevErrors => ({ ...prevErrors, [fieldName]: regexErrorText }));
+      } else if (fieldName === 'passwordCheck' && fields.password !== value) {
+        setErrors(prevErrors => ({ ...prevErrors, passwordCheck: fieldValidations[fieldName].regexErrorText }));
+      } else {
+        setErrors(prevErrors => ({ ...prevErrors, [fieldName]: '' }));
+      }
+    }
   };
 
-  const checkName = (value: string) => {
-    if (value.length === 0 || value === '') {
-      setNickname({ value: value, text: '닉네임을 입력해주세요.', hidden: false });
-      return;
-    }
-
-    let checkKor = /^[가-힣a-zA-Z]{2,10}$/;
-    if (!checkKor.test(value)) {
-      setNickname({
-        value: value,
-        text: '한글/영문으로 입력해주세요. (10글자 이하)',
-        hidden: false,
-      });
-      return;
-    }
-    setNickname({ value: value, text: '', hidden: true });
-  };
-
-  const checkPassword = (value: string) => {
-    if (value.length === 0 || value === '') {
-      setPassword({ value: value, text: '비밀번호를 입력해주세요.', hidden: false });
-      return;
-    }
-    let regPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\S{8,}$/;
-    if (!regPass.test(value)) {
-      setPassword({
-        value: value,
-        text: '공백없이 숫자, 대문자, 소문자가 하나씩 포함되어야 합니다. (8글자 이상)',
-        hidden: false,
-      });
-      return;
-    }
-    setPassword({ value: value, text: '', hidden: true });
-  };
-
-  const checkEmail = (value: string) => {
-    if (value.length === 0 || value === '') {
-      setEmail({ value: value, text: '이메일을 입력해주세요.', hidden: false });
-      return;
-    }
-    const emailRegex = /^[A-Za-z0-9]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(value) || !(value.length >= 5)) {
-      setEmail({ value: value, text: '이메일 주소를 정확히 입력해주세요.', hidden: false });
-      return;
-    }
-    setEmail({ value: value, text: '', hidden: true });
+  const onChangeField = (e: any, fieldName: string) => {
+    const { value } = e.target;
+    setFields(prevFields => ({ ...prevFields, [fieldName]: value }));
+    validateField(value, fieldName);
   };
 
   const isButtonDisabled =
-    email.hidden &&
-    nickname.hidden &&
-    password.hidden &&
-    passwordCheck.hidden &&
-    email.value !== '' &&
-    nickname.value !== '' &&
-    password.value !== '' &&
-    passwordCheck.value !== '' &&
-    password.value === passwordCheck.value;
+    Object.values(fields).some(value => value === '') ||
+    Object.values(errors).some(error => error !== '') ||
+    fields.password !== fields.passwordCheck;
 
-  const handleSignup = () => {
-    instance
-      .post('/users', {
-        email: email.value,
-        name: nickname.value,
-        password: password.value,
-        role: mode,
-      })
-      .then(res => {
-        if (res.data.success) {
-          console.log(res.data.message);
-          router.push(`/signup/success`);
-        } else {
-          console.log(res.data.message);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  const handleSignup = () =>
+    createMutation.mutate(
+      {
+        data: {
+          email: fields.email,
+          name: fields.nickname,
+          password: fields.password,
+          role: mode,
+        },
+      },
+      {
+        onSuccess: data => {
+          data.success && router.push(`/signup/pending`);
+        },
+        onError: error => {
+          console.log(error);
+          window.alert('회원가입에 실패했습니다.');
+        },
+      }
+    );
 
   return (
     <Container>
       <Header />
       <Box>
         <ChoiceBoxWrapper>
-          <SignUpBox mode={mode} setMode={setMode} />
+          <DetailBox
+            mode={mode}
+            setMode={setMode}
+            imgUrl="/images/Cat.svg"
+            imgUrl2="/images/People.svg"
+            memberName="모금자"
+            memberName2="후원자"
+            onClick1={() => setMode('ROLE_DONER')}
+            onClick2={() => setMode('ROLE_FUNDRAISER')}
+          />
         </ChoiceBoxWrapper>
         <InputBox>
-          <Name style={{ color: !email.hidden ? colors.red400 : colors.black }}>이메일</Name>
+          <Name style={{ color: errors.email ? colors.red400 : colors.black }}>이메일</Name>
           <Input
             id="email"
-            type={'email'}
+            type="email"
             placeholder="예) kitty@kitty.co.kr"
-            defaultValue={email.value}
-            onChange={(e: any) => checkEmail(e.target.value)}
+            value={fields.email}
+            onChange={e => onChangeField(e, 'email')}
           />
-          <HiddenMessage hidden={email.hidden}>{email.text}</HiddenMessage>
+          {errors.email && <HiddenMessage style={{ color: 'red' }}>{errors.email}</HiddenMessage>}
         </InputBox>
         <InputBox>
-          <Name style={{ color: !nickname.hidden ? colors.red400 : colors.black }}>
+          <Name style={{ color: errors.nickname ? colors.red400 : colors.black }}>
             닉네임
-            <Input
-              id="id"
-              type={'text'}
-              defaultValue={nickname.value}
-              onChange={(e: any) => checkName(e.target.value)}
-            />
-            <HiddenMessage hidden={nickname.hidden}>{nickname.text}</HiddenMessage>
+            <Input id="id" type="text" value={fields.nickname} onChange={e => onChangeField(e, 'nickname')} />
+            {errors.nickname && <HiddenMessage style={{ color: 'red' }}>{errors.nickname}</HiddenMessage>}
           </Name>
         </InputBox>
         <InputBox>
-          <Name style={{ color: !password.hidden ? colors.red400 : colors.black }}>
+          <Name style={{ color: errors.password ? colors.red400 : colors.black }}>
             비밀번호
-            <Input
-              id="password"
-              type={'password'}
-              defaultValue={password.value}
-              onChange={(e: any) => checkPassword(e.target.value)}
-            />
-            <HiddenMessage hidden={password.hidden}>{password.text}</HiddenMessage>
+            <Input id="password" type="password" value={fields.password} onChange={e => onChangeField(e, 'password')} />
+            {errors.password && <HiddenMessage style={{ color: 'red' }}>{errors.password}</HiddenMessage>}
           </Name>
         </InputBox>
         <InputBox>
-          <Name style={{ color: !passwordCheck.hidden ? colors.red400 : colors.black }}>
+          <Name style={{ color: errors.passwordCheck ? colors.red400 : colors.black }}>
             비밀번호 확인
             <Input
               id="password-check"
-              type={'password'}
-              defaultValue={passwordCheck.value}
-              onChange={onChangePasswordCheck}
+              type="password"
+              value={fields.passwordCheck}
+              onChange={e => onChangeField(e, 'passwordCheck')}
             />
-            <HiddenMessage hidden={passwordCheck.hidden}>{passwordCheck.text}</HiddenMessage>
+            {errors.passwordCheck && <HiddenMessage style={{ color: 'red' }}>{errors.passwordCheck}</HiddenMessage>}
           </Name>
-          {!passwordCheck.hidden && <HiddenMessage>비밀번호가 일치하지 않습니다.</HiddenMessage>}
         </InputBox>
         <ButtonBox>
           <BottomButton
-            title={'회원가입'}
-            width={'100%'}
-            height={'59px'}
-            backgroundColor={'${colors.pink500}'}
-            hoverFontColor={'${colors.white}'}
-            disabled={!isButtonDisabled}
+            title="회원가입"
+            width="100%"
+            height="59px"
+            backgroundColor={colors.pink500}
+            hoverFontColor={colors.white}
+            disabled={isButtonDisabled}
             onClick={handleSignup}
           />
         </ButtonBox>
