c534077f6a1b706871a8bd37ee55aa075a093db9
yeonkr <1714882@sookmyung.ac.kr>
2023-07-17 06:55:38 +0900

    add: 누락된 파일 추가

--- pages/mypage/fundraiser/index.tsx
+++ pages/mypage/fundraiser/index.tsx
@@ -5,9 +5,8 @@ import Image from 'next/image';
 import { colors } from '../../../styles/colors';
 import { useGetByEmail } from '../../../api/인증-인가-기부자-모금자-공통-api/인증-인가-기부자-모금자-공통-api';
 import { useGetBoards } from '../../../api/모금자용-권한용-api/모금자용-권한용-api';
-import { GetBoardsParams } from '../../../orval/model';
+import { GetBoardsParams, PageImageGet } from '../../../orval/model';
 import PostCard from '../../../components/PostCard/PostCard';
-import { AllPostResponse } from '../../../types/post';
 import { useRouter } from 'next/router';
 import ProfileImg from '../../../components/ProfileImg';
 import ProfileInfo from '../../../components/ProfileInfo';
@@ -21,7 +20,6 @@ export default function FundraiserMainProfile() {
   const router = useRouter();
   const getUserInfoStatus = useGetByEmail();
   const userInfo = getUserInfoStatus.data?.response;
-
   const getUserBoardStatus = useGetBoards(PAGE_PARAM);
   const userBoard = getUserBoardStatus.data?.response?.content;
 
@@ -46,7 +44,7 @@ export default function FundraiserMainProfile() {
       ) : (
         <>
           {userBoard?.map(board => (
-            <PostCard list={board as unknown as AllPostResponse} key={board.id} />
+            <PostCard list={board as unknown as PageImageGet} key={board.id} />
           ))}
         </>
       )}
@@ -59,64 +57,6 @@ export default function FundraiserMainProfile() {
 }
 
 const Container = styled.div``;
-
-const Thumbnail = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  align-content: center;
-  height: 131px;
-  background-color: ${colors.gray300};
-`;
-
-const ThumbnailList = styled.div`
-  width: 90%;
-  display: flex;
-  justify-content: space-between;
-  padding-top: 14px;
-`;
-
-const ProfileInfoComp = styled.div`
-  padding-left: 17px;
-`;
-const Profile = styled.div`
-  font-size: 18px;
-  font-weight: 700;
-  color: ${colors.black};
-  padding-top: 17px;
-`;
-
-const InformationBoxWrapper = styled.div`
-  padding-top: 30px;
-`;
-
-const InfoBox = styled.div`
-  position: relative;
-  padding-bottom: 33px;
-`;
-const InfoName = styled.div`
-  position: absolute;
-  left: 0;
-  display: inline;
-  width: 100px;
-  font-size: 14px;
-  font-weight: 600;
-`;
-
-const InfoValue = styled.div`
-  position: absolute;
-  left: 100px;
-  display: inline;
-  font-size: 14px;
-  font-weight: 600;
-`;
-
-const Line = styled.div`
-  width: 25rem;
-  padding: 28px 0 0 89px;
-  margin-left: 100px;
-  border-bottom: 1px solid #000;
-`;
 
 const DonationListTitle = styled.div`
   font-size: 18px;
